8e989ebebea6ea5e0a968ec3ba0c98b137fbc089
tedkatapetrov1 <teodor.petrov@abv.bg>
2025-05-28 13:42:13 +0300

    counting when clicking function

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ let count = 0;
 const countE1 = document.getElementById('count')
 const clickBtn = document.getElementById('clickBtn')
 const resetBtn = document.getElementById('resetBtn')
+
+// counting when clicking the button
+
+clickBtn.addEventListener('click', () => {
+    count++;
+    countE1.textContent = count;
+});
